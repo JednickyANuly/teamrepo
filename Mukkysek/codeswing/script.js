2b3cdb94c11dafb03e5615f750d5b92170cb9ad8
Krajda <mukky.sek9@gmail.com>
2024-11-14 15:21:41 +0100

    čtverec se hýbe

--- Mukkysek/codeswing/script.js
+++ Mukkysek/codeswing/script.js
@@ -1,10 +1,20 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+let poziceX = 0
 
-const kresli = ()=>{
+const kresli = ()=> {
+    ctx.clearRect(0, 0, canvas.width, canvas.height) ;
+    console.log(poziceX)
 ctx.fillStyle = "red";
-ctx.fillRect(0, 0, 150, 150);
+ctx.fillRect(poziceX, 0, 150, 150);
+poziceX = poziceX + 1
+if (poziceX > 100) {
+    poziceX = 0
+    
 }
+requestAnimationFrame(kresli)
+ }
 
-kresli()
+requestAnimationFrame(kresli)
+
