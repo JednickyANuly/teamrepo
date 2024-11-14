0e3889a58960ea07df37bde1d9ae1088f6d1417a
Krajda <mukky.sek9@gmail.com>
2024-11-14 15:33:09 +0100

    Kostka se hýbe podle šipek

--- Mukkysek/codeswing/script.js
+++ Mukkysek/codeswing/script.js
@@ -1,20 +1,35 @@
+// Objekt, který sleduje stav kláves
+let keys = {};
+
+// Funkce, která se spustí při stisknutí klávesy
+window.addEventListener("keydown", function(event) {
+  keys[event.code] = true;
+});
+
+// Funkce, která se spustí při uvolnění klávesy
+window.addEventListener("keyup", function(event) {
+  keys[event.code] = false;
+});
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 let poziceX = 0
 
-const kresli = ()=> {
+const kreslit = ()=> {
     ctx.clearRect(0, 0, canvas.width, canvas.height) ;
     console.log(poziceX)
 ctx.fillStyle = "red";
 ctx.fillRect(poziceX, 0, 150, 150);
-poziceX = poziceX + 1
-if (poziceX > 100) {
-    poziceX = 0
-    
+
+if (keys["ArrowLeft"]) {
+    poziceX -= 1
 }
-requestAnimationFrame(kresli)
- }
+if (keys["ArrowRight"]) {
+    poziceX += 1
+}
 
-requestAnimationFrame(kresli)
+requestAnimationFrame(kreslit)
+}
 
+requestAnimationFrame(kreslit)
