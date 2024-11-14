ffb013c80d1250eb8368fbb177c3cbf743985399
ondrissantos <o.prax@seznam.cz>
2024-11-14 15:29:32 +0100

    ovládání klávesama

--- ondra/kostka/script.js
+++ ondra/kostka/script.js
@@ -1,14 +1,42 @@
+// Objekt, který sleduje stav kláves
+let keys = {};
+
+// Funkce, která se spustí při stisknutí klávesy
+window.addEventListener("keydown", function(event) {
+  keys[event.code] = true;
+});
+
+// Funkce, která se spustí při uvolnění klávesy
+window.addEventListener("keyup", function(event) {
+  keys[event.code] = false;
+});
+
+
+
 // const platno = document.getElementById("platno")
 const ctx = platno.getContext("2d");
 
+let poziceX = 1
 
 const kreslit = ()=>{
+    ctx.clearRect(0,0, platno.width, platno.height);
+    console.log(poziceX)
 ctx.fillStyle = "blue";
-ctx.fillRect(0,0,150,200);
- }
-kreslit()
+ctx.fillRect  (poziceX,0,150,200);
+if (keys["ArrowLeft"]) {
+    poziceX -= 1
+}
+if (keys["ArrowRight"]) {
+    poziceX += 1
+}
+
+ requestAnimationFrame(kreslit)
+}
 
 
+
+
+ requestAnimationFrame(kreslit)
 
 
 
