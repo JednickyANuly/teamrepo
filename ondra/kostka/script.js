ef26c70bd7ca145440606c4079300b5a0ffe3451
ondrissantos <o.prax@seznam.cz>
2024-11-14 14:54:29 +0100

    kosticka

---
+++ ondra/kostka/script.js
@@ -0,0 +1,21 @@
+// const platno = document.getElementById("platno")
+const ctx = platno.getContext("2d");
+
+
+const kreslit = ()=>{
+ctx.fillStyle = "blue";
+ctx.fillRect(0,0,150,200);
+ }
+kreslit()
+
+
+
+
+
+
+
+
+
+
+
+
